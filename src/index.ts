372c889fbbd99e2cc2f094b9e887ed277f548809
jpascale <jpascale@itba.edu.ar>
2019-04-26 19:39:38 +0200

    Fix config load

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import * as async from 'async';
 import logging from './logging';
 import * as config from './config';
+const path = require('path');
 
 logging.info('Starting k8s monitor');
 
+if (process.argv.length <= 2) {
+  logging.error('No config file provided');
+  process.exit(1);
+}
+const configFilePath = path.resolve('.', process.argv[2]);
+
 // Load config
 logging.info('Loading config');
-const loadedConfig = config.loadConfig(process.argv);
-logging.info('Config loaded');
-
+const loadedConfig = config.loadConfig(configFilePath);
+logging.info('Config loaded ' + JSON.stringify(loadedConfig));
 
 // Start readiness and liveness, respond liveness, not readiness
 let ready = false;
