eac03c6072727f65593b7bace17bc2c7c8f91679
jpascale <jpascale@itba.edu.ar>
2019-04-26 18:12:54 +0200

    Add typescript config

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as async from 'async';
+
+console.log('I\'m a newborn project');
+
+// Load readiness and liveness, start responding liveness, not readiness
+// Load config
+// Load tasks
+// Load alerts
+// Start responding readiness
+// Start tasks loop
